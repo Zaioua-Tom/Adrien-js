132846958173a1999d5944b4a7e56d5b4c9a8934
Tom Zaioua <tom.zaioua@webedia-group.com>
2024-05-22 11:56:18 +0200

    etape-3-methode-1

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
 let images = ['virginie-1.webp', 'virginie-2.jpeg', 'virginie-3.jpg', 'virginie-4.webp', 'virginie-5.webp', 'virginie-6.jpeg', 'virginie-7.jpeg', 'virginie-8.jpeg', 'virginie-9.jpeg', 'virginie-10.jpeg'];
-let index = 0;
+let Image = ['virginie-1','virginie-2', 'virginie-3', 'virginie-4', 'virginie-5', 'virginie-6', 'virginie-7', 'virginie-8', 'virginie-9', 'virginie-10' ]
+let Name = ['virginie-1','virginie-2', 'virginie-3', 'virginie-4', 'virginie-5', 'virginie-6', 'virginie-7', 'virginie-8', 'virginie-9', 'virginie-10' ]
+let age = ['56','99', '66', '24', '56', '54', '33', '44', '38', '45' ]
 
+let index = 0;
 let carousel = document.querySelector('.carousel-container');
-images.forEach(image => {
+
+let json = JSON.stringify(images);
+console.log(json);
+images.forEach((image,i) => {
   let img = document.createElement('img');
   img.src = `images/${image}`;
   img.classList.add('carousel-item');
   img.style.display = 'none';
+  img.alt = `${Image[i]}`;
+  img.dataset.img = `${Name[i]}, ${age[i]}`;
   carousel.prepend(img);
 });
 
