1da081cf33c6011751c715f5ce43bbb79e6188e9
Tom Zaioua <tom.zaioua@webedia-group.com>
2024-05-28 10:44:43 +0200

    etape-4

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
 let carousel = document.querySelector('.carousel-container');
 let items = [];
-let images = JSON.parse(document.querySelector('.carousel-container').dataset.img);
+let path = document.querySelector('.carousel-container').dataset.path;
 let index = 0;
 
-images.forEach((image) => {
-  let img = document.createElement('img');
-  img.src = `images/${image}`;
-  img.classList.add('carousel-item');
-  img.style.display = 'none';
-  carousel.prepend(img);
-  items.push(img);
-});
-
-let totalItems = items.length;
-
-let counter = document.getElementById('counter');
-
-function updateCarousel() {
-  items.forEach(item => item.style.display = 'none');
-  items[index].style.display = 'block';
-  updateCounter();
+async function getImages() {
+  let response = await fetch(path);
+  let data = await response.json();
+  return data.images;
 }
+async function init() {
 
-function updateCounter() {
-  counter.textContent = `${index + 1} / ${totalItems}`;
+    const images = await getImages();
+
+    console.log(images);
+
+    images.forEach((image) => {
+    let img = document.createElement('img');
+    img.src = `${image}`;
+    img.classList.add('carousel-item');
+    img.style.display = 'none';
+    carousel.prepend(img);
+    items.push(img);
+    });
+
+    let totalItems = items.length;
+
+    let counter = document.getElementById('counter');
+
+    function updateCarousel() {
+    items.forEach(item => item.style.display = 'none');
+    items[index].style.display = 'block';
+    updateCounter();
+    }
+
+    function updateCounter() {
+    counter.textContent = `${index + 1} / ${totalItems}`;
+    }
+
+
+    document.getElementById('prev').addEventListener('click', function() {
+    index--;
+    if (index < 0) { index = totalItems - 1; }
+    updateCarousel();
+    });
+
+    document.getElementById('next').addEventListener('click', function() {
+    index++;
+    if (index === totalItems) { index = 0; }
+    updateCarousel();
+    });
+
+    updateCarousel(); 
 }
-
-
-document.getElementById('prev').addEventListener('click', function() {
-  index--;
-  if (index < 0) { index = totalItems - 1; }
-  updateCarousel();
-});
-
-document.getElementById('next').addEventListener('click', function() {
-  index++;
-  if (index === totalItems) { index = 0; }
-  updateCarousel();
-});
-
-updateCarousel(); 
+init();
